3b5e9369e1ab19db1c0aa1fd0086d9f788e33b48
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-08-28 12:40:30 -0700

    added drop down to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+// hide or display the dropdown menu
+  displayProfileDropDown = false;
+  showDropDown(){
+    if(this.displayProfileDropDown === false){
+      this.displayProfileDropDown  = true;
+    }else{
+      this.displayProfileDropDown = false;
+    }
+  }
 
   constructor() { }
 
