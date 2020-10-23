be08772c044084ca32d204e0d0ab646dfac39cf4
ike-njoku <ikenjokudc@gmail.com>
2020-10-23 13:33:32 -0700

    updated cart interface (currently reflects product and quantity)

---
+++ src/app/cartInterface.ts
@@ -0,0 +1,10 @@
+// import interface: product because the cart interface is going to have products
+import { Products } from './productInterface';
+export interface Cart{
+ product: Products;
+ productQuantity: number;
+}
+
+
+
+
