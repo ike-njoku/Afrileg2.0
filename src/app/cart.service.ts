978e12f7a0770dc77e7721646e9cdf9f54839d56
ike-njoku <ikenjokudc@gmail.com>
2020-10-30 12:55:18 -0700

    added in-app-memory-web-api to as a mock source for fetching cart ddata

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,22 +1,27 @@
-import { cart } from './mock-cart-delete';
 // import the cart interface
 import { Cart } from './cartInterface';
-import { HttpClient } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+// catching errors
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  // url to get cart details from
+  private cartDetailsUrl: string = 'api/cartDetails';
 
   constructor(private http: HttpClient) { }
 
   // get cart details should return an observable of cart details from
   // the server
   getCartDetails(): Observable<Cart[]>{
-    return of (cart);
+    // return as an observable of type cart interface, the information located in the url in the bracket (http.get())
+    return this.http.get<Cart[]>(this.cartDetailsUrl);
   }
 
   // remove product from cart
