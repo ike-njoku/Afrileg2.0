b20321ddd5de17918e10c3ddc016f33c2dfd9f39
ike-njoku <ikenjokudc@gmail.com>
2020-12-18 07:39:32 -0800

    updates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/afrilegplus'));
+app.use(express.static(__dirname + '/dist/afrileg2'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/afrilegplus/index.html'));
+        '/dist/afrileg2/index.html'));
 });
 app.listen(process.env.PORT || 8080);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'afrilegplus'`, () => {
+  it(`should have as title 'afrileg2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('afrilegplus');
+    expect(app.title).toEqual('afrileg2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('afrilegplus app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('afrileg2 app is running!');
   });
 });
